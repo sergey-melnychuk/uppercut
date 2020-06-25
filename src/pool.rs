3cc3be5857207032e787932bb8427a51d80f8135
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2020-06-25 17:14:46 +0200

    Use crossbeam_channel::unbounded instead of std::sync::mpsc::channel

--- src/pool.rs
+++ src/pool.rs
@@ -1,7 +1,6 @@
-use std::sync::mpsc::{channel, Sender, Receiver};
+use crossbeam_channel::{unbounded, Sender, Receiver};
 use std::thread;
 use std::thread::JoinHandle;
-use std::sync::{Arc, Mutex};
 
 extern crate core_affinity;
 use core_affinity::CoreId;
@@ -25,9 +24,7 @@ impl ThreadPool {
     }
 
     pub fn new(size: usize) -> ThreadPool {
-        let (sender, receiver) = channel();
-        let receiver = Arc::new(Mutex::new(receiver));
-
+        let (sender, receiver) = unbounded();
         let mut workers = Vec::with_capacity(size);
 
         let core_ids = core_affinity::get_core_ids().unwrap();
@@ -37,7 +34,7 @@ impl ThreadPool {
             } else {
                 None
             };
-            let worker = Worker::new(Arc::clone(&receiver), core_id);
+            let worker = Worker::new(receiver.clone(), core_id);
             workers.push(worker);
         }
 
@@ -86,13 +83,13 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(receiver: Arc<Mutex<Receiver<Job>>>, core_id: Option<CoreId>) -> Worker {
+    fn new(receiver: Receiver<Job>, core_id: Option<CoreId>) -> Worker {
         let thread = thread::spawn(move || {
             if let Some(id) = core_id {
                 core_affinity::set_for_current(id);
             }
             loop {
-                let job = receiver.lock().unwrap().recv();
+                let job = receiver.recv();
                 match job {
                     Ok(Job::Task(f)) => f(),
                     Ok(Job::Stop) => break,
