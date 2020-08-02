b482e5f8a9a84432bb02f2fff2f390b2b1260237
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2020-08-02 13:27:03 +0200

    Add remote example

---
+++ examples/remote.rs
@@ -0,0 +1,54 @@
+use uppercut::pool::ThreadPool;
+use uppercut::config::{Config, RemoteConfig, SchedulerConfig};
+use uppercut::core::System;
+use uppercut::api::{Envelope, AnyActor, AnySender};
+
+#[derive(Default)]
+struct PingPong {
+    count: usize,
+}
+
+impl AnyActor for PingPong {
+    fn receive(&mut self, envelope: Envelope, sender: &mut dyn AnySender) {
+        if let Some(s) = envelope.message.downcast_ref::<String>() {
+            self.count += 1;
+
+            println!("{}: {}", sender.me(), s);
+            if s == "ping" {
+                let r = Envelope::of("pong".to_string(), sender.me());
+                sender.send(&envelope.from, r);
+            } else if s == "pong" {
+                let r = Envelope::of("ping".to_string(), sender.me());
+                sender.send(&envelope.from, r);
+            }
+        }
+    }
+}
+
+fn main() {
+    let cores = num_cpus::get();
+    let pool = ThreadPool::new(2 * (cores + 2) + 2);
+
+    // sys 1
+
+    let cfg1 = Config::new(
+        SchedulerConfig::with_total_threads(cores),
+        RemoteConfig::listening_at("0.0.0.0:9001"));
+    let sys1 = System::new(&cfg1);
+    let run1 = sys1.run(&pool).unwrap();
+
+    run1.spawn_default::<PingPong>("pong");
+
+    // sys 2
+
+    let cfg2 = Config::new(
+        SchedulerConfig::with_total_threads(cores),
+        RemoteConfig::listening_at("0.0.0.0:9002"));
+    let sys2 = System::new(&cfg2);
+    let run2 = sys2.run(&pool).unwrap();
+
+    run2.spawn_default::<PingPong>("ping");
+    run2.send("pong@127.0.0.1:9001", Envelope::of(b"ping", "ping"));
+
+    std::thread::park();
+}
