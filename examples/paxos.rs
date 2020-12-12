0b9ace351f422afa327efb3806b3e7828243e080
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2020-12-12 13:52:03 +0100

    PAXOS example: add (de)serialization for messages

--- examples/paxos.rs
+++ examples/paxos.rs
@@ -4,12 +4,15 @@ use std::time::Duration;
 extern crate chrono;
 use chrono::Local;
 
+extern crate bytes;
+use bytes::{Bytes, Buf, BytesMut, BufMut};
+
 extern crate uppercut;
 use uppercut::api::{AnyActor, Envelope, AnySender};
 use uppercut::core::System;
 use uppercut::pool::ThreadPool;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 enum Message {
     Request { val: u64 },
     Prepare { seq: u64 },
@@ -19,21 +22,71 @@ enum Message {
     Accepted { seq: u64, val: u64 },
     Rejected { seq: u64 },
     Selected { seq: u64, val: u64 },
+    Empty,
 }
 
-// TODO
-// impl Into<Vec<u8>> for Message {
-//     fn into(self) -> Vec<u8> {
-//         Vec::new()
-//     }
-// }
+impl Into<Vec<u8>> for Message {
+    fn into(self) -> Vec<u8> {
+        let mut buf = BytesMut::with_capacity(1 + 8 + 8);
+        match self {
+            Message::Request { val } => { buf.put_u8(1); buf.put_u64(val); },
+            Message::Prepare { seq } => { buf.put_u8(2); buf.put_u64(seq); },
+            Message::Promise { seq } => { buf.put_u8(3); buf.put_u64(seq); },
+            Message::Ignored { seq, hi } => { buf.put_u8(4); buf.put_u64(seq); buf.put_u64(hi); },
+            Message::Accept { seq, val } => { buf.put_u8(5); buf.put_u64(seq); buf.put_u64(val); },
+            Message::Accepted { seq, val } => { buf.put_u8(6); buf.put_u64(seq); buf.put_u64(val); },
+            Message::Rejected { seq } => { buf.put_u8(7); buf.put_u64(seq); },
+            Message::Selected { seq, val } => { buf.put_u8(8); buf.put_u64(seq); buf.put_u64(val); },
+            Message::Empty => buf.put_u8(0)
+        };
+        buf.split().to_vec()
+    }
+}
 
-// TODO
-// impl From<&Vec<u8>> for Message {
-//     fn from(_: &Vec<u8>) -> Self {
-//         Message::Empty(0)
-//     }
-// }
+impl From<Vec<u8>> for Message {
+    fn from(buf: Vec<u8>) -> Self {
+        let mut buf = Bytes::from(buf);
+        let op = buf.get_u8();
+        match op {
+            1 => Message::Request { val: buf.get_u64() },
+            2 => Message::Prepare { seq: buf.get_u64() },
+            3 => Message::Promise { seq: buf.get_u64() },
+            4 => Message::Ignored { seq: buf.get_u64(), hi: buf.get_u64() },
+            5 => Message::Accept { seq: buf.get_u64(), val: buf.get_u64() },
+            6 => Message::Accepted { seq: buf.get_u64(), val: buf.get_u64() },
+            7 => Message::Rejected { seq: buf.get_u64() },
+            8 => Message::Selected { seq: buf.get_u64(), val: buf.get_u64() },
+            _ => Message::Empty
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_into_from_bytes() {
+        let messages = vec![
+            Message::Request { val: 42 },
+            Message::Prepare { seq: 42 },
+            Message::Promise { seq: 42 },
+            Message::Ignored { seq: 42, hi: 100500 },
+            Message::Accept { seq: 42, val: 0xCAFEBABEDEADBEEF },
+            Message::Accepted { seq: 42, val: 0xCAFEBABEDEADBEEF },
+            Message::Rejected { seq: 42 },
+            Message::Selected { seq: 42, val: 0xCAFEBABEDEADBEEF },
+            Message::Empty,
+        ];
+
+        for message in messages {
+            let buf: Vec<u8> = message.clone().into();
+            let msg: Message = buf.into();
+            assert_eq!(msg, message);
+        }
+    }
+}
+
 
 #[derive(Default)]
 struct Agent {
@@ -59,9 +112,8 @@ impl Agent {
 
     fn handle(&mut self, message: Message, from: String) -> Vec<(String, Message)> {
         match message {
-            Message::Request { val } => {
+            Message::Request { val } => { // TODO propagate `val` from `Request` to `Selected`
                 self.seq += 1;
-                self.val = val;
                 self.others()
                     .into_iter()
                     .map(|addr| (addr, Message::Prepare { seq: self.seq }))
@@ -122,9 +174,11 @@ impl Agent {
             Message::Rejected { seq: _ } => {
                 vec![]
             },
-            Message::Selected { seq: _, val: _ } => {
+            Message::Selected { seq: _, val } => {
+                self.val = val;
                 vec![]
-            }
+            },
+            _ => vec![]
         }
     }
 
@@ -145,14 +199,29 @@ enum Control {
 impl AnyActor for Agent {
     fn receive(&mut self, envelope: Envelope, sender: &mut dyn AnySender) {
         if let Some(msg) = envelope.message.downcast_ref::<Message>() {
-            println!("{} actor={} message={:?}", self.time(), sender.me(), msg);
+            println!("{} actor={} message/local={:?}", self.time(), sender.me(), msg);
             self.handle(msg.clone(), envelope.from)
                 .into_iter()
                 .for_each(|(target, msg)| {
-                    let envelope = Envelope::of(msg)
+                    let buf: Vec<u8> = msg.into();
+                    let envelope = Envelope::of(buf)
                         .to(&target)
                         .from(sender.me());
-                    sender.delay(&target, envelope, Duration::from_millis(self.delay_millis));
+                    let delay = Duration::from_millis(self.delay_millis);
+                    sender.delay(&target, envelope, delay);
+                });
+        } else if let Some(buf) = envelope.message.downcast_ref::<Vec<u8>>() {
+            let msg: Message = buf.to_owned().into();
+            println!("{} actor={} message/parse={:?}", self.time(), sender.me(), msg);
+            self.handle(msg.clone(), envelope.from)
+                .into_iter()
+                .for_each(|(target, msg)| {
+                    let buf: Vec<u8> = msg.into();
+                    let envelope = Envelope::of(buf)
+                        .to(&target)
+                        .from(sender.me());
+                    let delay = Duration::from_millis(self.delay_millis);
+                    sender.delay(&target, envelope, delay);
                 });
         } else if let Some(ctrl) = envelope.message.downcast_ref::<Control>() {
             match ctrl {
@@ -187,7 +256,8 @@ fn main() {
     let values = vec![42, 101, 13];
     for val in values {
         let msg = Message::Request { val };
-        let envelope = Envelope::of(msg);
+        let buf: Vec<u8> = msg.into();
+        let envelope = Envelope::of(buf);
         run.send(peers.get(0).unwrap(), envelope);
     }
 
