adc4a6370bd875521c6f8d83ef9680012c9e0de3
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2021-02-22 12:36:57 +0100

    Update basic example and README

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,8 +2,11 @@ use std::sync::mpsc::{channel, Sender};
 
 extern crate uppercut;
 use uppercut::api::{AnyActor, Envelope, AnySender};
+use uppercut::config::Config;
 use uppercut::core::System;
 use uppercut::pool::ThreadPool;
+use std::thread::sleep;
+use std::time::Duration;
 
 #[derive(Debug)]
 struct Message(usize, Sender<usize>);
@@ -12,24 +15,27 @@ struct Message(usize, Sender<usize>);
 struct State;
 
 impl AnyActor for State {
-    fn receive(&mut self, envelope: Envelope, _sender: &mut dyn AnySender) {
+    fn receive(&mut self, envelope: Envelope, sender: &mut dyn AnySender) {
         if let Some(msg) = envelope.message.downcast_ref::<Message>() {
-            println!("Actor 'State' received a message: {}", msg.0);
+            sender.log(&format!("received: {:?}", msg));
             msg.1.send(msg.0).unwrap();
         }
     }
 }
 
 fn main() {
-    let sys = System::default();
+    let cfg = Config::default();
+    let sys = System::new("basic", "localhost", &cfg);
     let pool = ThreadPool::new(6);
     let run = sys.run(&pool).unwrap();
 
-    run.spawn_default::<State>("x");
+    run.spawn_default::<State>("state");
 
     let (tx, rx) = channel();
-    run.send("x", Envelope::of(Message(42, tx)));
+    run.send("state", Envelope::of(Message(42, tx)));
 
-    rx.recv().unwrap();
+    sleep(Duration::from_secs(3));
+    let result = rx.recv().unwrap();
+    println!("result: {}", result);
     run.shutdown();
 }
